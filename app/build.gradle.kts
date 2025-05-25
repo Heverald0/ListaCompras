13341e4b036816bfd71b7bc28a417f8ec69373c1
Heveraldo Augusto Silva Viana Filho <Heveraldoa8@gmail.com>
2025-05-24 23:23:08 -0300

    Alteração de configurações da Build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,3 @@
-plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
-}
-
 android {
     namespace = "br.com.livrokotlin.listadecompras"
     compileSdk = 35
@@ -26,24 +21,17 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
     kotlinOptions {
         jvmTarget = "11"
     }
 
-}
-
-dependencies {
-
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    buildFeatures {
+        viewBinding = true
+    }
 }
